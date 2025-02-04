5c08d2e03f41a2a754aba5f0e565a517d3fda77b
BBPOLYZ <116862908+BBPOLYZ@users.noreply.github.com>
2025-02-05 07:04:59 +0800

    Update 4_Statistical Failure Patterns.py

--- pages/4_Statistical Failure Patterns.py
+++ pages/4_Statistical Failure Patterns.py
@@ -21,11 +21,11 @@ def calculate_metrics(df, index_var):
     
     return table_combined
 
-# Function to load data
+# Function to load data from file path
 @st.cache_data
-def load_data(uploaded_file):
+def load_data(file_path):
     try:
-        return pd.read_excel(uploaded_file)
+        return pd.read_excel(file_path)
     except Exception as e:
         st.error(f"Error loading file: {e}")
         return None
@@ -34,24 +34,16 @@ def load_data(uploaded_file):
 
 st.title('Failure Rate and Time-to-Failure Trends in Hong Kong')
 
-# Sidebar for file uploader and user inputs
-st.sidebar.header('Upload Data and Select Variables')
-uploaded_files = st.sidebar.file_uploader("Choose Excel files", accept_multiple_files=True, type=["xlsx"])
+# Sidebar for user inputs
+st.sidebar.header('Select Variables')
+file_path = st.sidebar.text_input('Enter the file path of your data:', 'BURSTS_FW_CT_1.xlsx')
 
-if uploaded_files:
-    dataframes = []
-    for uploaded_file in uploaded_files:
-        df = load_data(uploaded_file)
-        if df is not None:
-            dataframes.append((uploaded_file.name, df))
-            st.sidebar.write(f"Uploaded file: {uploaded_file.name}")
-            st.sidebar.dataframe(df.head())  # Display only the first few rows
+if file_path:
+    data = load_data(file_path)
 
-    if dataframes:
-        # Debug: Print file names and dataframe info
-        for file_name, df in dataframes:
-            st.write(f"File name: {file_name}")
-            st.write(df.info())
+    if data is not None:
+        st.sidebar.write("Data Preview:")
+        st.sidebar.dataframe(data.head())  # Display only the first few rows
 
         # User input for variable name
         selected_var = st.sidebar.text_input('Enter Categorical Variable for Analysis')
@@ -65,54 +57,28 @@ if uploaded_files:
 
             with tab1:
                 st.subheader('Data Analysis')
-                for file_name, df in dataframes:
-                    if selected_var in df.columns:
-                        table_metrics = calculate_metrics(df, selected_var)
-                        st.subheader(f'Data Analysis for {file_name}')
-                        st.dataframe(table_metrics)
+                if selected_var in data.columns:
+                    table_metrics = calculate_metrics(data, selected_var)
+                    st.dataframe(table_metrics)
 
             with tab2:
                 st.subheader('Bar Plot')
                 if comparison_type == 'Failure Rate':
-                    for file_name, df in dataframes:
-                        if selected_var in df.columns:
-                            table_metrics = calculate_metrics(df, selected_var)
-                            st.write(f'Bar Plot for {file_name}')
-                            fig, ax = plt.subplots(figsize=(8, 4))
-                            if f'bursts/km_{selected_var}' in table_metrics.columns:
-                                sns.barplot(x=table_metrics.index, y=f'bursts/km_{selected_var}', data=table_metrics, ax=ax, palette="viridis")
-                                ax.set_ylabel('bursts/km')
-                            elif f'leaks/km_{selected_var}' in table_metrics.columns:
-                                sns.barplot(x=table_metrics.index, y=f'leaks/km_{selected_var}', data=table_metrics, ax=ax, palette="magma")
-                                ax.set_ylabel('leaks/km')
-                            ax.set_title(f'Bar Plot for {selected_var} in {file_name}')
-                            ax.set_xlabel(selected_var)
-                            st.pyplot(fig)
-
-                    # Add a third chart for comparison of FW and SW
-                    if len(dataframes) >= 2:
-                        df1_name, df1 = dataframes[0]
-                        df2_name, df2 = dataframes[1]
-                        if selected_var in df1.columns and selected_var in df2.columns:
-                            table_metrics1 = calculate_metrics(df1, selected_var)
-                            table_metrics2 = calculate_metrics(df2, selected_var)
-                            combined_metrics = pd.concat([table_metrics1.assign(Dataset='FW'), table_metrics2.assign(Dataset='SW')])
-
-                            st.write('Comparison of FW and SW')
-                            fig, ax = plt.subplots(figsize=(10, 6))
-                            if f'bursts/km_{selected_var}' in combined_metrics.columns:
-                                sns.barplot(x=combined_metrics.index, y=f'bursts/km_{selected_var}', hue='Dataset', data=combined_metrics, ax=ax, palette="viridis")
-                                ax.set_ylabel('bursts/km')
-                            elif f'leaks/km_{selected_var}' in combined_metrics.columns:
-                                sns.barplot(x=combined_metrics.index, y=f'leaks/km_{selected_var}', hue='Dataset', data=combined_metrics, ax=ax, palette="magma")
-                                ax.set_ylabel('leaks/km')
-                            ax.set_title(f'Comparison of FW and SW for {selected_var}')
-                            ax.set_xlabel(selected_var)
-                            st.pyplot(fig)
+                    if selected_var in data.columns:
+                        table_metrics = calculate_metrics(data, selected_var)
+                        fig, ax = plt.subplots(figsize=(8, 4))
+                        if f'bursts/km_{selected_var}' in table_metrics.columns:
+                            sns.barplot(x=table_metrics.index, y=f'bursts/km_{selected_var}', data=table_metrics, ax=ax, palette="viridis")
+                            ax.set_ylabel('bursts/km')
+                        elif f'leaks/km_{selected_var}' in table_metrics.columns:
+                            sns.barplot(x=table_metrics.index, y=f'leaks/km_{selected_var}', data=table_metrics, ax=ax, palette="magma")
+                            ax.set_ylabel('leaks/km')
+                        ax.set_title(f'Bar Plot for {selected_var}')
+                        ax.set_xlabel(selected_var)
+                        st.pyplot(fig)
                 else:
-                    combined_df = pd.concat([df.assign(Dataset=file_name) for file_name, df in dataframes])
                     fig, ax = plt.subplots(figsize=(10, 6))
-                    sns.barplot(x='Failure Year', y='LENGTH', hue='Dataset', data=combined_df, ax=ax, palette="viridis")
+                    sns.barplot(x='Failure Year', y='LENGTH', data=data, ax=ax, palette="viridis")
                     ax.set_title(f'Comparative Bar Plot for {selected_var}')
                     ax.set_xlabel('Failure Year')
                     ax.set_ylabel('LENGTH')
@@ -120,37 +86,35 @@ if uploaded_files:
 
             with tab3:
                 st.subheader('Box Plot')
-                for file_name, df in dataframes:
-                    if selected_var in df.columns:
-                        fig, ax = plt.subplots(figsize=(8, 4))
-                        sns.boxplot(data=df, x=selected_var, y='LENGTH', hue='FAULT_TYPE', ax=ax)
-                        ax.set_title(f'Box Plot for {selected_var} in {file_name}')
-                        ax.set_xlabel(selected_var)
-                        ax.set_ylabel('Length')
-                        st.pyplot(fig)
+                if selected_var in data.columns:
+                    fig, ax = plt.subplots(figsize=(8, 4))
+                    sns.boxplot(data=data, x=selected_var, y='LENGTH', hue='FAULT_TYPE', ax=ax)
+                    ax.set_title(f'Box Plot for {selected_var}')
+                    ax.set_xlabel(selected_var)
+                    ax.set_ylabel('Length')
+                    st.pyplot(fig)
 
             with tab4:
                 st.subheader('Heatmap')
-                for file_name, df in dataframes:
-                    if selected_var in df.columns:
-                        failure_counts = df.pivot_table(values='LENGTH', index='Failure Year', columns=selected_var, aggfunc='count').fillna(0)
-                        total_lengths = df.groupby(selected_var)['LENGTH'].sum()
-                        heatmap_data = failure_counts.div(total_lengths, axis=1) * 100  # Scaling factor
+                if selected_var in data.columns:
+                    failure_counts = data.pivot_table(values='LENGTH', index='Failure Year', columns=selected_var, aggfunc='count').fillna(0)
+                    total_lengths = data.groupby(selected_var)['LENGTH'].sum()
+                    heatmap_data = failure_counts.div(total_lengths, axis=1) * 100  # Scaling factor
 
-                        fig, ax = plt.subplots(figsize=(10, 6))
-                        sns.heatmap(heatmap_data, annot=True, cmap='coolwarm', ax=ax)
-                        cbar = ax.collections[0].colorbar
-                        cbar.set_label('Failures per km (x10^-2)')
-                        cbar.ax.yaxis.set_major_formatter(ScalarFormatter())
-                        cbar.ax.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
-                        cbar.ax.yaxis.get_offset_text().set_position((-0.1, 0))
-                        cbar.ax.yaxis.get_offset_text().set_fontsize(10)
-                        ax.set_title(f'Heatmap for {selected_var} in {file_name} by Year')
-                        st.pyplot(fig)
-                    else:
-                        st.error(f"Selected variable '{selected_var}' not found in one of the datasets")
+                    fig, ax = plt.subplots(figsize=(10, 6))
+                    sns.heatmap(heatmap_data, annot=True, cmap='coolwarm', ax=ax)
+                    cbar = ax.collections[0].colorbar
+                    cbar.set_label('Failures per km (x10^-2)')
+                    cbar.ax.yaxis.set_major_formatter(ScalarFormatter())
+                    cbar.ax.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+                    cbar.ax.yaxis.get_offset_text().set_position((-0.1, 0))
+                    cbar.ax.yaxis.get_offset_text().set_fontsize(10)
+                    ax.set_title(f'Heatmap for {selected_var} by Year')
+                    st.pyplot(fig)
+                else:
+                    st.error(f"Selected variable '{selected_var}' not found in the dataset")
         else:
-            st.error(f"No numeric data available for the selected variable '{selected_var}' in {file_name}.")
+            st.error(f"No numeric data available for the selected variable '{selected_var}'.")
     else:
         st.error(f"The variable '{selected_var}' is not found in the uploaded file.")
 else:
